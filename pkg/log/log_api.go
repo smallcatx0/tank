fc9fd9a3d93ae69a186aab9e768ffff43edc7bad
谭奎 <1312308948@qq.com>
2021-05-10 14:46:16 +0800

    完善单元测试

--- pkg/log/log_api.go
+++ pkg/log/log_api.go
@@ -23,7 +23,7 @@ func DebugF(requestID, template string, args ...interface{}) {
 
 func DebugT(requestID, msg string, extra ...interface{}) {
 	extSlice := make([]string, 0, len(extra))
-	for one := range extra {
+	for _, one := range extra {
 		tmpStr, _ := json.Marshal(one)
 		extSlice = append(extSlice, string(tmpStr))
 	}
@@ -46,29 +46,128 @@ func InfoF(requestID, template string, args ...interface{}) {
 
 func InfoT(requestID, msg string, extra ...interface{}) {
 	extSlice := make([]string, 0, len(extra))
-	for one := range extra {
+	for _, one := range extra {
 		tmpStr, _ := json.Marshal(one)
 		extSlice = append(extSlice, string(tmpStr))
 	}
-	ZapLoger.Debug(msg, zap.String("request_id", requestID), zap.Strings("extra", extSlice))
+	ZapLoger.Info(msg, zap.String("request_id", requestID), zap.Strings("extra", extSlice))
 }
 
 func Warn(msg string, extra ...string) {
-	ZapLoger.Warn(msg, zap.Strings("extra", extra))
+	var request_id string
+	if len(extra) >= 1 {
+		request_id = extra[0]
+		extra = extra[1:]
+	}
+	ZapLoger.Warn(msg, zap.String("request_id", request_id), zap.Strings("extra", extra))
+}
+
+func WarnF(requestID, template string, args ...interface{}) {
+	msg := fmt.Sprintf(template, args...)
+	ZapLoger.Warn(msg, zap.String("request_id", requestID))
+}
+
+func WarnT(requestID, msg string, extra ...interface{}) {
+	extSlice := make([]string, 0, len(extra))
+	for _, one := range extra {
+		tmpStr, _ := json.Marshal(one)
+		extSlice = append(extSlice, string(tmpStr))
+	}
+	ZapLoger.Warn(msg, zap.String("request_id", requestID), zap.Strings("extra", extSlice))
 }
 
 func Error(msg string, extra ...string) {
-	ZapLoger.Error(msg, zap.Strings("extra", extra))
+	var request_id string
+	if len(extra) >= 1 {
+		request_id = extra[0]
+		extra = extra[1:]
+	}
+	ZapLoger.Error(msg, zap.String("request_id", request_id), zap.Strings("extra", extra))
+}
+
+func ErrorF(requestID, template string, args ...interface{}) {
+	msg := fmt.Sprintf(template, args...)
+	ZapLoger.Error(msg, zap.String("request_id", requestID))
+}
+
+func ErrorT(requestID, msg string, extra ...interface{}) {
+	extSlice := make([]string, 0, len(extra))
+	for _, one := range extra {
+		tmpStr, _ := json.Marshal(one)
+		extSlice = append(extSlice, string(tmpStr))
+	}
+	ZapLoger.Error(msg, zap.String("request_id", requestID), zap.Strings("extra", extSlice))
 }
 
 func DPanic(msg string, extra ...string) {
-	ZapLoger.DPanic(msg, zap.Strings("extra", extra))
+	var request_id string
+	if len(extra) >= 1 {
+		request_id = extra[0]
+		extra = extra[1:]
+	}
+	ZapLoger.DPanic(msg, zap.String("request_id", request_id), zap.Strings("extra", extra))
+}
+
+func DPanicF(requestID, template string, args ...interface{}) {
+	msg := fmt.Sprintf(template, args...)
+	ZapLoger.DPanic(msg, zap.String("request_id", requestID))
+}
+
+func DPanicT(requestID, msg string, extra ...interface{}) {
+	extSlice := make([]string, 0, len(extra))
+	for _, one := range extra {
+		tmpStr, _ := json.Marshal(one)
+		extSlice = append(extSlice, string(tmpStr))
+	}
+	ZapLoger.DPanic(msg, zap.String("request_id", requestID), zap.Strings("extra", extSlice))
 }
 
 func Panic(msg string, extra ...string) {
-	ZapLoger.Panic(msg, zap.Strings("extra", extra))
+	var request_id string
+	if len(extra) >= 1 {
+		request_id = extra[0]
+		extra = extra[1:]
+	}
+	ZapLoger.Panic(msg, zap.String("request_id", request_id), zap.Strings("extra", extra))
+}
+
+func PanicF(requestID, template string, args ...interface{}) {
+	msg := fmt.Sprintf(template, args...)
+	ZapLoger.Panic(msg, zap.String("request_id", requestID))
+}
+
+func PanicT(requestID, msg string, extra ...interface{}) {
+	extSlice := make([]string, 0, len(extra))
+	for _, one := range extra {
+		tmpStr, _ := json.Marshal(one)
+		extSlice = append(extSlice, string(tmpStr))
+	}
+	ZapLoger.Panic(msg, zap.String("request_id", requestID), zap.Strings("extra", extSlice))
 }
 
 func Fatal(msg string, extra ...string) {
-	ZapLoger.Fatal(msg, zap.Strings("extra", extra))
+	var request_id string
+	if len(extra) >= 1 {
+		request_id = extra[0]
+		extra = extra[1:]
+	}
+	ZapLoger.Fatal(msg, zap.String("request_id", request_id), zap.Strings("extra", extra))
+}
+
+func FatalF(requestID, template string, args ...interface{}) {
+	msg := fmt.Sprintf(template, args...)
+	ZapLoger.Fatal(msg, zap.String("request_id", requestID))
+}
+
+func FatalT(requestID, msg string, extra ...interface{}) {
+	extSlice := make([]string, 0, len(extra))
+	for _, one := range extra {
+		tmpStr, _ := json.Marshal(one)
+		extSlice = append(extSlice, string(tmpStr))
+	}
+	ZapLoger.Fatal(msg, zap.String("request_id", requestID), zap.Strings("extra", extSlice))
+}
+
+func Sync() {
+	ZapLoger.Sync()
 }
--- pkg/log/log_test.go
+++ pkg/log/log_test.go
@@ -1,11 +1,50 @@
-package glog
+package glog_test
 
-import "testing"
+import (
+	"testing"
+
+	glog "gitee.com/smallcatx0/gtank/pkg/log"
+)
 
 func setUp() {
-
 }
 func TestFile(t *testing.T) {
 	setUp()
+	glog.InitLog2file("/home/logs/tank/curr.log", "Info")
+	param := map[string]interface{}{
+		"name": "kui",
+		"age":  18,
+	}
 
+	glog.Debug("该条日志不应会被记录")
+	glog.SetAtomLevel("debug")
+
+	glog.Debug("test debug")
+	glog.Debug("test debug with requestId", "request-12dfawedfse")
+	glog.Debug("test debug with mor", "request-12dfawedfse", "extra one", "extra two")
+	glog.DebugF("requestid", "测试模板日志age=%d", 23)
+	glog.DebugT("requestid", "DebugT", param, param)
+
+	glog.Info("测试INFO 级别完整信息", "request-123123123", "扩展信息1", "扩展信息2")
+	glog.InfoF("", "测试模板日志name=%s", "kui")
+	glog.InfoT("", "测试模板日志Json扩展信息", param, param)
+
+	glog.Warn("测试warn级别完整信息", "request-123123123", "扩展信息1", "扩展信息2")
+	glog.WarnF("", "测试模板日志name=%s", "kui")
+	glog.WarnT("", "测试模板日志Json扩展信息", param, param)
+
+	glog.Error("测试warn级别完整信息", "request-123123123", "扩展信息1", "扩展信息2")
+	glog.ErrorF("", "测试模板日志name=%s", "kui")
+	glog.ErrorT("", "测试模板日志Json扩展信息", param, param)
+
+	glog.DPanic("测试DPanic级别完整信息", "request-dadfmwesd", "扩展信息1", "扩展信息2")
+
+	glog.Sync()
+}
+
+func TestCons(t *testing.T) {
+	glog.InitLog2std("info")
+	glog.Debug("不会被打出来的日志")
+	glog.SetAtomLevel("debug")
+	glog.Debug("会被打出来的 debug日志", "rq-1231dsf", "extra1", "extra2")
 }
