6a12cf04901afd9d1bea919e575013d812433904
少猫子 <elbarrytan@outlook.com>
2024-09-06 14:28:54 +0800

    excel writer

--- go.mod
+++ go.mod
@@ -14,6 +14,8 @@ require (
 	github.com/smallcatx0/gequest v1.2.0
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.8.4
+	github.com/tidwall/gjson v1.7.5
+	github.com/xuri/excelize/v2 v2.8.1
 	go.uber.org/zap v1.27.0
 	gorm.io/driver/mysql v1.1.0
 	gorm.io/gorm v1.21.9
@@ -43,28 +45,35 @@ require (
 	github.com/mitchellh/mapstructure v1.1.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/pelletier/go-toml v1.2.0 // indirect
 	github.com/pkg/errors v0.8.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/richardlehane/mscfb v1.0.4 // indirect
+	github.com/richardlehane/msoleps v1.0.3 // indirect
 	github.com/shoenig/go-m1cpu v0.1.6 // indirect
 	github.com/spf13/afero v1.1.2 // indirect
 	github.com/spf13/cast v1.3.0 // indirect
 	github.com/spf13/jwalterweatherman v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.3 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
+	github.com/tidwall/match v1.0.3 // indirect
+	github.com/tidwall/pretty v1.1.0 // indirect
 	github.com/tklauser/go-sysconf v0.3.11 // indirect
 	github.com/tklauser/numcpus v0.6.0 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
+	github.com/xuri/efp v0.0.0-20231025114914-d1ff6096ae53 // indirect
+	github.com/xuri/nfp v0.0.0-20230919160717-d98342af3f05 // indirect
 	github.com/yusufpapurcu/wmi v1.2.3 // indirect
 	go.opentelemetry.io/otel v0.19.0 // indirect
 	go.opentelemetry.io/otel/metric v0.19.0 // indirect
 	go.opentelemetry.io/otel/trace v0.19.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
-	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
-	golang.org/x/sys v0.10.0 // indirect
-	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/protobuf v1.23.0 // indirect
 	gopkg.in/ini.v1 v1.51.0 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
---
+++ pkg/excel/excel_recorder.go
@@ -0,0 +1,167 @@
+package excel
+
+import (
+	"fmt"
+	"gtank/pkg/helper"
+	"strconv"
+
+	"github.com/tidwall/gjson"
+	"github.com/xuri/excelize/v2"
+)
+
+type ExcelRecorder struct {
+	FilePath         string
+	ExcelFp          *excelize.File
+	Sheet            string
+	Limit            int
+	Page             int
+	FileNameTemplate string
+}
+
+func NewExcelRecorder(path string) *ExcelRecorder {
+	instance := &ExcelRecorder{
+		FilePath: path,
+		ExcelFp:  excelize.NewFile(),
+		Sheet:    "Sheet1",
+	}
+	return instance
+}
+
+func NewExcelRecorderPage(template string, limit int) *ExcelRecorder {
+	ins := &ExcelRecorder{
+		ExcelFp:          excelize.NewFile(),
+		Sheet:            "Sheet1",
+		Page:             1,
+		Limit:            limit,
+		FileNameTemplate: template,
+	}
+	return ins
+}
+
+// JsonListWrite 将json列表写入excel中
+func (e *ExcelRecorder) JsonListWrite(p Pos, jsonStr string, isFirst bool) Pos {
+	result := gjson.Parse(jsonStr)
+	keys := make([]string, 0)
+	for i, line := range result.Array() {
+		if i == 0 {
+			keys = JsonKeys(line.String())
+			if !isFirst {
+				// 不写首行数据
+				continue
+			}
+		}
+		p = e.JsonWrite(p, keys, line.String())
+	}
+	return p
+}
+
+// JsonWrite 单条json 按照keys的键顺序写入excel
+// ajson格式 `{"key1":"val1", "key2":"val2"}`
+func (e *ExcelRecorder) JsonWrite(p Pos, keys []string, ajson ...string) Pos {
+	for _, line := range ajson {
+		aline := gjson.Parse(line).Value().(map[string]interface{})
+		lineValues := Map2Arr(aline, keys)
+		e.ExcelFp.SetSheetRow(e.Sheet, p.String(), &lineValues)
+		p.Y += 1
+	}
+	return p
+}
+
+// Save 保存
+func (e *ExcelRecorder) Save() error {
+	if e.FileNameTemplate != "" {
+		e.FilePath = fmt.Sprintf(e.FileNameTemplate, e.Page)
+	}
+	helper.TouchDir(e.FilePath)
+	if err := e.ExcelFp.SaveAs(e.FilePath); err != nil {
+		return err
+	}
+	return nil
+}
+
+func List2Arrs(lines []map[string]interface{}, keys []string) [][]interface{} {
+	ret := make([][]interface{}, 0, len(lines))
+	for _, line := range lines {
+		tmp := Map2Arr(line, keys)
+		ret = append(ret, tmp)
+	}
+	return ret
+}
+
+// Map2Arr 根据keys顺序 将字典转为数组
+func Map2Arr(amap map[string]interface{}, keys []string) []interface{} {
+	ret := make([]interface{}, 0, len(amap))
+	for _, key := range keys {
+		if v, ok := amap[key]; ok {
+			ret = append(ret, v)
+		} else {
+			ret = append(ret, nil)
+		}
+	}
+	return ret
+}
+
+// 分页写入
+func (e *ExcelRecorder) WritePagpenate(p Pos, lineJson string, htable string, isFirst bool) Pos {
+	lines := gjson.Parse(lineJson).Array()
+	if htable == "" {
+		htable = lines[0].String()
+		lines = lines[1:]
+	}
+	keys := JsonKeys(htable)
+	// 第一次调用 需要写表头
+	if isFirst {
+		p = e.JsonWrite(p, keys, htable)
+	}
+	for _, aline := range lines {
+		if p.Y >= e.Limit {
+			// 保存文件
+			e.Save()
+			// 新生成文件
+			e.ExcelFp = excelize.NewFile()
+			e.Page += 1
+			p.Y = 1
+			// 写表头
+			p = e.JsonWrite(p, keys, htable)
+		}
+		p = e.JsonWrite(p, keys, aline.String())
+	}
+	return p
+}
+
+type Pos struct {
+	X, Y           int
+	Row, Col, Addr string
+}
+
+// Convert 坐标转Excel地址
+func (p *Pos) Convert() {
+	p.Col = x2col(p.X)
+	p.Row = strconv.Itoa(p.Y)
+	p.Addr = p.Col + p.Row
+}
+
+func (p *Pos) String() string {
+	// 先转化
+	p.Convert()
+	return p.Addr
+}
+
+func x2col(x int) string {
+	result := ""
+	for x > 0 {
+		x--
+		result = string(rune(x%26+'A')) + result
+		x = x / 26
+	}
+	return result
+}
+
+func JsonKeys(json string) []string {
+	keys := make([]string, 0)
+	gjson.Parse(json).ForEach(func(key, value gjson.Result) bool {
+		keys = append(keys, key.String())
+		return true
+	})
+	return keys
+}
